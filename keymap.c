8a6d23ac07a408b63bcc2697c8ae9baca7913d8b
Andoni Serrano <hola@andoniserra.me>
2020-10-12 19:41:44 +0200

    layout update

--- keymap.c
+++ keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Lower
    * ,-----------------------------------------------------------------------------------.
-   * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  |   `  |
+   * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Del  |
    * |------+------+------+------+------+------+------+------+------+------+------+------|
    * |  Del |      |      |      |      |      |      |   _  |   +  |   {  |   }  |  \   |
    * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -35,27 +35,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-----------------------------------------------------------------------------------'
    */
 [_LOWER] = LAYOUT_ortho_4x12( \
-  KC_TILD,  KC_EXLM,  KC_AT,  KC_HASH,  KC_DLR,   KC_PERC,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_GRV,  \
+  KC_TILD,  KC_EXLM,  KC_AT,  KC_HASH,  KC_DLR,   KC_PERC,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_DEL,  \
   KC_DEL,   KC_NO,    KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_UNDS,  KC_PLUS,  KC_LCBR,  KC_RCBR,  KC_BSLS, \
   KC_NO,    KC_NO,    KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_MINS,  KC_EQL,   KC_LBRC,  KC_RBRC,  KC_PIPE, \
-  KC_NO,    KC_NO,    KC_NO,  KC_NO,    _______,  KC_NO,    KC_NO,    _______,  KC_NO,    KC_NO,    KC_NO,    KC_NO    \
+  KC_NO,    KC_NO,    KC_NO,  KC_NO,    _______,  KC_SPC,   KC_SPC,   _______,  KC_NO,    KC_NO,    KC_NO,    KC_NO    \
 ),
 
   /* Raise
    * ,-----------------------------------------------------------------------------------.
-   * |      |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | DEL  |
+   * |  `   |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | DEL  |
    * |------+------+------+------+------+------+------+------+------+------+------+------|
-   * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |      | Left |  Up  | Down |Right |      |
+   * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |      | Home | PgDn | PgUp | End  |      |
    * |------+------+------+------+------+------+------+------+------+------+------+------|
-   * |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |      |      |      |      |      |      |
+   * |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |      |      |  ,   |  .   |      |Enter |
    * |------+------+------+------+------+------+------+------+------+------+------+------|
-   * |      |      |      |      |      |      |autosh|      | Home | PgUp | PgDn | End  |
+   * |      |      |      |      |      |      |autosh|      | Left | Down |  Up  |Right |
    * `-----------------------------------------------------------------------------------'
    */
 [_RAISE] = LAYOUT_ortho_4x12( \
-    KC_NO,   KC_1,   KC_2,   KC_3,   KC_4,     KC_5,    KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_DEL, \
-    KC_F1,   KC_F2,  KC_F3,  KC_F4,  KC_F5,    KC_F6,   KC_NO,    KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_NO,  \
-    KC_F7,   KC_F8,  KC_F9,  KC_F10, KC_F11,   KC_F12,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,  \
-    KC_NO,   KC_NO,  KC_NO,  KC_NO,  _______,  KC_NO,   KC_ASTG,  _______,    KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END  \
+    KC_GRV,  KC_1,   KC_2,   KC_3,   KC_4,     KC_5,    KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_DEL, \
+    KC_F1,   KC_F2,  KC_F3,  KC_F4,  KC_F5,    KC_F6,   KC_NO,    KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END,   KC_NO,  \
+    KC_F7,   KC_F8,  KC_F9,  KC_F10, KC_F11,   KC_F12,  KC_NO,    KC_NO,    KC_COMM,  KC_DOT,   KC_NO,    KC_ENT,  \
+    KC_NO,   KC_NO,  KC_NO,  KC_NO,  _______,  KC_NO,   KC_ASTG,  _______,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT  \
 ),
 };
